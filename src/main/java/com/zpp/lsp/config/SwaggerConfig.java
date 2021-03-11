efb6bfdead24aac994c13c37343cc91e1cbb4b4f
hssj-cx-003 <2573787653@qq.com>
2021-03-11 13:49:34 +0800

    --备注： 1.swagger关闭生产访问 2.dev和prod环境配置

--- src/main/java/com/zpp/lsp/config/SwaggerConfig.java
+++ src/main/java/com/zpp/lsp/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.zpp.lsp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,9 +20,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Value("${swagger.enable}")
+    private boolean enableSwagger;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enableSwagger)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.zpp.lsp.controller"))
