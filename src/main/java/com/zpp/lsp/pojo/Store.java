2cec9f72ae971f838d6dc8db775c2d5f2c46828c
hssj-cx-003 <2573787653@qq.com>
2021-03-18 10:29:12 +0800

    --备注： 1.store实体设置日期格式

--- src/main/java/com/zpp/lsp/pojo/Store.java
+++ src/main/java/com/zpp/lsp/pojo/Store.java
@@ -1,7 +1,9 @@
 package com.zpp.lsp.pojo;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -27,8 +29,12 @@ public class Store {
 
     private String storePhone;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     private String salt;
